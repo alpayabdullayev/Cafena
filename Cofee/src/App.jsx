5fbe4e91ec308e0247e72c95e25b733b69372649
alpayabdullayev <alpayabdullayev0@gmail.com>
2023-11-29 12:49:17 +0400

    title componnets

--- Cofee/src/App.jsx
+++ Cofee/src/App.jsx
@@ -7,7 +7,6 @@ function App() {
   return (
     <>
     
-<h2>\dsafdf</h2>
 
     </>
   )
---
+++ Cofee/src/components/SpecialMenu/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import Title from '../Title'
+
+const SpecialMenu = () => {
+  return (
+    <>
+    
+    <section id='specialMenu'>  
+        <div className="container">
+            <Title title={"SPECIAL MENU"} titleh1={"CAFENA POPULAR MENU"} />
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default SpecialMenu
