42c70a3e6eb624d735fc4bfc32367f8684b408f5
Ahmad Baghirov <tu5r38eh9@code.edu.az>
2023-12-01 23:21:52 +0400

    FAQ added

---
+++ Cofee/src/pages/FAQ/index.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import "./index.scss"
+import Footer from '../../layout/Footer'
+import PageTitle from '../../components/PageTitle'
+import FAQitem from '../../components/FAQitem'
+
+function FAQ() {
+    
+    return (
+        <>
+            <PageTitle title={"FAQ"} />
+            <section id='faq'>
+                <div className="container">
+                    <div className="faqbox">
+                        <FAQitem title="How does this work?" content="Simply put, we deliver your favourite menu from coffee house of choice. Our platform asking enables you to enter your postcode and select a nearby restaurant of choice e.g McDonaldsKFC, …Wagamama e.t.c for delivery. Within each of these Coffee you will find their menus which you can add to basket."/>
+                        <FAQitem title="Some menus are missing on a coffee shop page?" content="Simply put, we deliver your favourite menu from coffee house of choice. Our platform asking enables you to enter your postcode and select a nearby restaurant of choice e.g McDonaldsKFC, …Wagamama e.t.c for delivery. Within each of these Coffee you will find their menus which you can add to basket." />
+                        <FAQitem title="Do any of your cheeses contain enzymes derived from animal sources?" content="Simply put, we deliver your favourite menu from coffee house of choice. Our platform asking enables you to enter your postcode and select a nearby restaurant of choice e.g McDonaldsKFC, …Wagamama e.t.c for delivery. Within each of these Coffee you will find their menus which you can add to basket." />
+                        <FAQitem title="Are your doughs vegan or vegetarian friendly?" content="Simply put, we deliver your favourite menu from coffee house of choice. Our platform asking enables you to enter your postcode and select a nearby restaurant of choice e.g McDonaldsKFC, …Wagamama e.t.c for delivery. Within each of these Coffee you will find their menus which you can add to basket." />
+                    </div>
+                </div>
+            </section>
+            <Footer linkvalue={"#faq"} />
+        </>
+    )
+}
+
+export default FAQ
--- Cofee/src/pages/Home/index.jsx
+++ Cofee/src/pages/Home/index.jsx
@@ -1,40 +1,40 @@
-import React from "react";
-import SpecialMenu from "../../components/SpecialMenu";
-import News from "../../components/News";
-import BasketSideBar from "../../components/BasketSideBar";
-import WishlistSideBar from "../../components/WishlistSideBar";
-import ButtonWishlistAndBasket from "../../components/ButtonWishlistAndBasket";
-import FilterProvider from "../../context/FilterContext";
-import Header from "../../components/Header";
-import Overlay from "../../components/Overlay";
-import AboutUs from '../../components/AboutUs'
-import TopGrade from "../../components/TopGrade";
-import Testimonial from "../../components/Testimonial";
-import Footer from "../../layout/Footer";
-import Years from "../../components/Years";
+  import React from "react";
+  import SpecialMenu from "../../components/SpecialMenu";
+  import News from "../../components/News";
+  import BasketSideBar from "../../components/BasketSideBar";
+  import WishlistSideBar from "../../components/WishlistSideBar";
+  import ButtonWishlistAndBasket from "../../components/ButtonWishlistAndBasket";
+  import FilterProvider from "../../context/FilterContext";
+  import Header from "../../components/Header";
+  import Overlay from "../../components/Overlay";
+  import AboutUs from '../../components/AboutUs'
+  import TopGrade from "../../components/TopGrade";
+  import Testimonial from "../../components/Testimonial";
+  import Footer from "../../layout/Footer";
+  import Years from "../../components/Years";
 
 
 
-const Home = () => {
-  return (
-    <div>
-      <>
-      <Overlay/>
-         <BasketSideBar />
-        <WishlistSideBar />
-        <Header/>
-        <Years/>
-        <AboutUs/>
-        <FilterProvider>
-          <SpecialMenu />
-        </FilterProvider>
-        <Testimonial/>
-        <TopGrade/>
-        <News/>
-        <Footer/>
-      </>
-    </div>
-  );
-};
+  const Home = () => {
+    return (
+      <div>
+        <>
+        <Overlay/>
+          <BasketSideBar />
+          <WishlistSideBar />
+          <Header/>
+          <Years/>
+          <AboutUs/>
+          <FilterProvider>
+            <SpecialMenu />
+          </FilterProvider>
+          <Testimonial/>
+          <TopGrade/>
+          <News/>
+          <Footer linkvalue={"#header"}/>
+        </>
+      </div>
+    );
+  };
 
-export default Home;
+  export default Home;
